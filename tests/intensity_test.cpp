4a030a5ba79addc04d471362666d387570450f1d
Daniel Duberg <danielduberg@gmail.com>
2024-09-26 22:22:15 +0200

    Removed color and image

--- tests/intensity_test.cpp
+++ tests/intensity_test.cpp
@@ -1,11 +1,10 @@
 // UFO
-#include <ufo/core/color.hpp>
+#include <ufo/core/intensity.hpp>
 
 // Catch2
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-namespace ufo
-{
-TEST_CASE("Color") { ufo::Color color; }
-}  // namespace ufo
+using namespace ufo;
+
+TEST_CASE("Intensity") { Intensity intensity; }
